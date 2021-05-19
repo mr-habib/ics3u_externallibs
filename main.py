7bf662e940e70fdc2dea47b6c9e0e9ccf1b450a6
Mr. Habib <sirmrhabib@gmail.com>
2021-05-19 09:43:42 -0400

    Updated for Q4 2021

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 # mm/dd/yyyy
 # Descriptive title
 # Note, I WILL be marking comments on this assignment! (out of 2)
+
+def calc_days_until(date_string):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
